8e08666cb9d24b9d07d76a8fd10570f2e19ff230
Eduardo Pizarro <eduardo-pizarro@hotmail.es>
2023-03-01 16:57:29 +0100

    done without styles

---
+++ src/components/ProductTable.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import ProductRow from './ProductRow';
+
+function ProductTable(props) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.products.map((product) => (
+          <ProductRow
+            {...product}
+          />
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default ProductTable;
